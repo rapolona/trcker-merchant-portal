565742d0ce473f03dda1496e51731bc4b3b4cc82
itloghugisbilog <jet.gutierrez@gmail.com>
2020-08-26 14:54:18 +0800

    Revert "Develop"

--- Frontend/trckr/config/trckr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'backend_url' => "http://ec2-54-161-105-93.compute-1.amazonaws.com:6001/",
-    'capability_url' => "http://ec2-54-161-105-93.compute-1.amazonaws.com:6002/",
-
-    "table_pagination" => "20"
-    ];
