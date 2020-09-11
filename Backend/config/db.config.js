1f7e46a8470823b3c7bd65840e7a6314d7bf2b98
Duke Quing <dtwq13@gmail.com>
2020-09-11 11:14:56 +0800

    fix merge conflict

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   HOST: "localhost",
-<<<<<<< Updated upstream
-  USER: "tester",
-  PASSWORD: "password",
-  DB: "task-redesign",
-=======
   USER: "root",
   PASSWORD: "123456",
   DB: "newdb",
->>>>>>> Stashed changes
   dialect: "mysql",
   pool: {
     max: 5,
