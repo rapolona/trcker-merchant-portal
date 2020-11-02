b72f61803ba0788e3ec87210c9fd07237881d970
mcsanantonio <mcsanantonio@gmail.com>
2020-11-03 02:30:42 +0800

    Temporarily included the HTML files in the resources folder. Will need to apply it to existing pages.

---
+++ Frontend/trckr/resources/views/concrete/footer.blade.php
@@ -0,0 +1,108 @@
+ <div class="sidebar sidebar-lg scroller scroller-vertical">
+        <div class="p-4">
+          <h3>Find Customer</h3>
+          <hr>
+          <div class="row row-15 align-items-end">
+            <div class="col-12 col-md-4 col-lg-12">
+              <div class="input-group form-group">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="basic-addon11"><span class="fa-user"></span></label>
+                </div>
+                <input class="form-control" id="basic-addon11" type="text" name="id" placeholder="Customer ID#">
+              </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-12">
+              <div class="input-group form-group">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="basic-addon12"><span class="fa-user"></span></label>
+                </div>
+                <input class="form-control" id="basic-addon12" type="text" name="cusName" placeholder="Customer Name">
+              </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-12">
+              <div class="input-group form-group">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="basic-addon13"><span class="fa-envelope"></span></label>
+                </div>
+                <input class="form-control" id="basic-addon13" type="email" name="cusEmail" placeholder="Customer Email">
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-12">
+              <p>Search Country</p>
+              <select class="form-control">
+                <option value="0" selected="selected">Filter by Location</option>
+                <option value="1">United States</option>
+                <option value="2">Europe</option>
+                <option value="3">Asia</option>
+                <option value="4">India</option>
+              </select>
+            </div>
+            <div class="col-12 col-md-6 col-lg-12">
+              <p>Search Company</p>
+              <select class="form-control">
+                <option value="0" selected="selected">Filter by Company</option>
+                <option value="1">Apple</option>
+                <option value="2">Sony</option>
+                <option value="3">Envato</option>
+                <option value="4">Microsoft</option>
+                <option value="5">Google</option>
+              </select>
+            </div>
+            <div class="col-12 col-md-4 col-lg-12">
+              <div class="row row-10">
+                <div class="col-12">
+                  <p>Registration Date</p>
+                </div>
+                <div class="col-12">
+                  <div class="input-group">
+                    <div class="input-group-prepend"><span class="input-group-text"><span class="fa-calendar"></span></span></div>
+                    <input class="form-control" type="text" name="datetime" data-datetimepicker='{"widgetPositioning":{"horizontal":"right"}}'>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="input-group">
+                    <div class="input-group-prepend"><span class="input-group-text"><span class="fa-calendar"></span></span></div>
+                    <input class="form-control" type="text" name="datetime" data-datetimepicker='{"widgetPositioning":{"horizontal":"right"}}'>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <hr>
+              <div class="group-10">
+                <button class="btn btn-primary" type="submit">Search</button>
+                <div class="d-inline-block">
+                  <div class="custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" id="voinhmhw"/>
+                    <label class="custom-control-label" for="voinhmhw">Save Search
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="template-panel">
+        <div class="template-panel-heading">Template Panel
+          <button class="template-panel-switch fa-cog" data-multi-switch='{"targets":".template-panel","scope":".template-panel","isolate":"[data-multi-switch]"}'></button>
+        </div>
+        <div class="template-panel-body">
+          <div class="template-panel-group">
+            <button class="theme-switch fa-check active" title="Navbar Theme" style="border-color: #b5b5b5 #b5b5b5 #4b4a4a #4b4a4a; color: white;" data-theme-switch="concrete-navbar-default" data-theme='{"navbar-color":"#fff","navbar-bg":"#3d3c3c","navbar-hover-color":"#fff","navbar-hover-bg":"#fff","navbar-title-color":"#adb5bd","navbar-panel-bg":"#b5b5b5","navbar-brand-invert":"0%"}'></button>
+            <button class="theme-switch fa-check" title="Navbar Theme" style="border-color: #29d999 #29d999 #1a1f21 #1a1f21; color: white;" data-theme-switch="concrete-navbar-success" data-theme='{"navbar-color":"#fff","navbar-bg":"#1a1f21","navbar-hover-color":"#fff","navbar-hover-bg":"#ededed","navbar-title-color":"#adb5bd","navbar-panel-bg":"#29d999","navbar-brand-invert":"0%"}'></button>
+            <button class="theme-switch fa-check" title="Navbar Theme" style="border-color: #3e84d7 #3e84d7 #1a1f21 #1a1f21; color: white;" data-theme-switch="concrete-navbar-primary" data-theme='{"navbar-color":"#fff","navbar-bg":"#1a1f21","navbar-hover-color":"#fff","navbar-hover-bg":"#ededed","navbar-title-color":"#adb5bd","navbar-panel-bg":"#3e84d7","navbar-brand-invert":"0%"}'></button>
+            <button class="theme-switch fa-check" title="Navbar Theme" style="border-color: #fff #fff #666 #666; color: black;" data-theme-switch="concrete-navbar-light" data-theme='{"navbar-color":"#666","navbar-bg":"#efefef","navbar-hover-color":"#333","navbar-hover-bg":"#333","navbar-title-color":"#333","navbar-panel-bg":"#eee","navbar-brand-invert":"100%"}'></button>
+          </div>
+          <ul class="template-panel-list">
+            <ul class="template-panel-item"><a class="template-panel-link" href="../admindex/index.html">Admindex<span class="template-panel-popup"><img class="template-panel-preview" src="images/templates/template-01-400x174.png"></span></a></ul>
+            <ul class="template-panel-item"><a class="template-panel-link" href="../adminifix/index.html">Adminifix<span class="template-panel-popup"><img class="template-panel-preview" src="images/templates/template-02-400x174.png"></span></a></ul>
+            <ul class="template-panel-item"><a class="template-panel-link" href="../emerald/index.html">Emerald<span class="template-panel-popup"><img class="template-panel-preview" src="images/templates/template-03-400x174.png"></span></a></ul>
+            <ul class="template-panel-item"><a class="template-panel-link" href="../iodashboard/index.html">IoDashboard<span class="template-panel-popup"><img class="template-panel-preview" src="images/templates/template-04-400x174.png"></span></a></ul>
+            <ul class="template-panel-item"><a class="template-panel-link active" href="../concrete/index.html">Concrete<span class="template-panel-popup"><img class="template-panel-preview" src="images/templates/template-05-400x174.png"></span></a></ul>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
