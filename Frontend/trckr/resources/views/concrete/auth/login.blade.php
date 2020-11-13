07e45eebbc90d2be91721d6d8181bc40b4df6fed
mcsanantonio <mcsanantonio@gmail.com>
2020-11-13 23:47:54 +0800

    Revision on the login form and style

--- Frontend/trckr/resources/views/concrete/auth/login.blade.php
+++ Frontend/trckr/resources/views/concrete/auth/login.blade.php
@@ -27,7 +27,7 @@
     </div>
 
     <div class="wrap-input100 validate-input m-b-16">
-        <input class="input100" type="password" name="pass" id="pass" placeholder="Password">
+        <input class="input100" type="password" name="password" id="pass" placeholder="Password">
         <span class="focus-input100"></span>
         
     </div>
@@ -38,51 +38,4 @@
         </button>
     </div>
 </form>
-<!-- <div class="col-lg-5">
-    <div class="row row-10 align-items-end">
-        <div class="col-6 col-sm-7 logo"><a  href="#"><img src="{{ config('concreteadmin.logo_img', 'logo_img')  }}" alt=""></a></div>
-    </div>
-    <form class="panel" method="post" action="{{ config('concreteadmin.login_url', 'login')  }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-lg-12 order-lg-1">
-                    @if($errors->has('email'))
-                        <div class="alert alert-dismissible alert-danger alert-sm" role="alert"><span class="alert-icon fa-remove"></span><span>{{ $errors->first('email') }}</span>
-                            <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span class="fa-close" aria-hidden="true"></span></button>
-                        </div>
-                    @endif
-
-                    @if($expiredMsg)
-                        <div class="alert alert-dismissible alert-{{ $expiredMsg['type'] }} alert-sm" role="alert"></span><span>{{ $expiredMsg['message'] }}</span>
-                            <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span class="fa-close" aria-hidden="true"></span></button>
-                        </div>
-                    @endif
-
-                    <div class="form-group">
-                        <label for="user">Username</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend"><span class="input-group-text fa fa-user"></span></div>
-                            <input class="form-control" id="email" type="text" name="email" placeholder="Enter username">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="pass">Password</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend"><span class="input-group-text fa fa-lock"></span></div>
-                            <input class="form-control" id="pass" type="password" name="password" placeholder="Enter password">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="panel-footer">
-            <div class="row row-10">
-                <div class="col-sm-12 text-sm-right">
-                    <button class="btn btn-primary" type="submit">Sign In</button>
-                </div>
-            </div>
-        </div>
-    </form>
-</div> -->
 @endsection
