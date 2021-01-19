60998946619877f8463f966c1eafd2b155ebb527
KyleBarts <kpibartido@gmail.com>
2021-01-19 20:43:49 +0800

    Added pagination to payout requests

--- Backend/app/controllers/user_payout_request.controller.js
+++ Backend/app/controllers/user_payout_request.controller.js
@@ -5,18 +5,24 @@ const Op = db.Sequelize.Op;
 
 // Retrieve all Payout requests from the database.
 exports.findAll = (req, res) => {
+    var page_number = 1;
+    var count_per_page = 25;
+    if((req.query.page)&&(req.query.count_per_page)){
+        var page_number = parseInt(req.query.page);
+        var count_per_page = parseInt(req.query.count_per_page);
+        
+      }
+    var skip_number_of_items = (page_number * count_per_page) - count_per_page;
 
-
-
-  User_payout_request.findAll()
+    User_payout_request.findAndCountAll({offset:skip_number_of_items, limit: count_per_page})
     .then(data => {
-      res.send(data);
+        res.send(data);
     })
     .catch(err => {
-      res.status(500).send({
+        res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving products."
-      });
+            err.message || "Some error occurred while retrieving products."
+        });
     });
 };
 
