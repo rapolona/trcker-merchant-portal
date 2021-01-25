c864d89f406bb4643f580c24351fd0cf6fbb7e83
rob <rapolona@yondu.com>
2021-01-25 23:46:11 +0800

    fixes

--- Frontend/trckr/resources/views/concrete/auth/passwords/forgotPin.blade.php
+++ Frontend/trckr/resources/views/concrete/auth/passwords/forgotPin.blade.php
@@ -1,7 +1,7 @@
 @extends('concrete.layouts.custom')
 
 @section('content')
-    <form class="login100-form validate-form" method="post" action="{{ url('forgot-password') }}">
+    <form class="login100-form validate-form" method="post" action="{{ url('forgot-password') }}"  id="pwd-container">
         <span class="login100-form-title">
             <img src="{{ config('concreteadmin.logo_img', 'logo_img')  }}" alt="Hustle Logo">
         </span>
@@ -17,17 +17,21 @@
 
 
             <div class="wrap-input100 validate-input m-b-16">
-                <input class="input100" type="text" name="email_address" id="email" placeholder="email">
+                <input class="input100" type="text" name="passwordToken" id="passwordToken" placeholder="PIN">
                 <span class="focus-input100"></span>
             </div>
 
             <div class="wrap-input100 validate-input m-b-16">
-                <input class="input100" type="text" name="email_address" id="email" placeholder="email">
+                <input class="input100" type="password" name="password" id="password" placeholder="New Password">
                 <span class="focus-input100"></span>
             </div>
 
+            <div>
+                <div class="pwstrength_viewport_progress"></div>
+            </div>
+
             <div class="wrap-input100 validate-input m-b-16">
-                <input class="input100" type="text" name="email_address" id="email" placeholder="email">
+                <input class="input100" type="password" name="password_confirmation" id="password_confirmation" placeholder="Retype Password">
                 <span class="focus-input100"></span>
             </div>
 
@@ -36,3 +40,27 @@
             </div>
     </form>
 @endsection
+
+@section('js')
+    <script src="{{ asset('js/pwstrength-bootstrap.min.js') }}"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            "use strict";
+            var options = {};
+            options.ui = {
+                container: "#pwd-container",
+                viewports: {
+                    progress: ".pwstrength_viewport_progress"
+                },
+                showVerdictsInsideProgressBar: true
+            };
+            options.common = {
+                debug: true,
+                onLoad: function () {
+                    $('#messages').text('Start typing password');
+                }
+            };
+            $('#password').pwstrength(options);
+        });
+    </script>
+@stop
