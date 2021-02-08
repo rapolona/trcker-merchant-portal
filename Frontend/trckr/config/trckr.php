016e2a3bd2f0e74c995ae26cbb9f6cbb1c6c0d4f
rob <rapolona@yondu.com>
2021-02-08 21:21:57 +0800

    new api url

--- Frontend/trckr/config/trckr.php
+++ Frontend/trckr/config/trckr.php
@@ -2,9 +2,10 @@
 
 return [
 
-    //'backend_url' => "http://ec2-54-161-105-93.compute-1.amazonaws.com:6001/",
-    //'capability_url' => "http://ec2-54-161-105-93.compute-1.amazonaws.com:6002/",
-    'backend_url' => "https://hustle-portal.com/",
-    'capability_url' => "https://hustle-portal.com/",
-    "table_pagination" => "20"
+    'backend_url' => "http://ec2-3-90-170-202.compute-1.amazonaws.com:6001/",
+    'capability_url' => "http://ec2-3-90-170-202.compute-1.amazonaws.com:6002/",
+
+    //'backend_url' => "https://hustle-portal.com/",
+    //'capability_url' => "https://hustle-portal.com/",
+    "table_pagination" => "25"
     ];
