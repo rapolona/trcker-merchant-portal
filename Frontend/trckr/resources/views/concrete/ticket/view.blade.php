3c2afc6bf60db7fa9b78b87245c785c0f39dde1f
rob <rapolona@yondu.com>
2021-03-06 19:07:31 +0800

    remove nex and pre

--- Frontend/trckr/resources/views/concrete/ticket/view.blade.php
+++ Frontend/trckr/resources/views/concrete/ticket/view.blade.php
@@ -52,13 +52,14 @@
                             
                         </div>
                         <div class="col-md-2 pull-right text-right">
-                            @if(isset($pagination->prev))
+                            <?php /*
+                             @if(isset($pagination->prev))
                             <a href="{{ url('ticket/view/' . $pagination->prev->campaign_id . '/' . $pagination->prev->task_ticket_id ) }}?page={{ isset($pagination->prev->page)? $pagination->prev->page : ($pagination->next->page - 1) }}&{!! http_build_query($pagination->filter) !!}" class="btn btn-primary"><span class="fa-arrow-circle-left"></span></a>
                             @endif
 
                             @if($pagination->next)
                             <a href="{{ url('ticket/view/' . $pagination->next->campaign_id . '/' . $pagination->next->task_ticket_id ) }}?page={{ isset($pagination->next->page)? $pagination->next->page : ($pagination->prev->page + 1) }}&{!! http_build_query($pagination->filter) !!}" class="btn btn-primary"><span class="fa-arrow-circle-right"></span></a>
-                            @endif
+                            @endif*/ ?>
                         </div>
                     
                 </div>
