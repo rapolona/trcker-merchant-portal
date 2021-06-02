cf6777bb2b21169fbdbdbfa202c10af1d82361e4
rapolona <roberto.apolona@gmail.com>
2021-06-02 21:48:54 +0800

    asdf
    dfsdf

--- Frontend/trckr/resources/views/concrete/respondent/edit.blade.php
+++ Frontend/trckr/resources/views/concrete/respondent/edit.blade.php
@@ -33,9 +33,25 @@
                         <div class="input-group form-group">
                             <div class="input-group-prepend"><span class="input-group-text"><span class="fa-map-marker"></span></span></div>
                             <input class="form-control {{ $errors->first('latitude')? 'form-control-danger' : '' }}" type="text" name="latitude" value="{{ old('latitude') }}" placeholder="Latitude">
-
-                            <select name="city" id="my-city-dropdown"></select>
                         </div>
+
+                                <div>
+            <table>
+                <tr>
+                    <td>Region</td>
+                    <td><select id="region"></select></td>
+                </tr>
+                <tr>
+                    <td>Province</td>
+                    <td><select id="province"></select></td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td><select id="city"></select></td>
+                </tr>
+            </table>
+
+        </div>
                     </div>
 
                     <div class="col-sm-12 text-right">
@@ -51,6 +67,40 @@
 @section('js')
     <script type="text/javascript" src="{{url('/js/jquery.ph-locations-v1.0.0.js')}}"></script>
     <script type="text/javascript">
+            var my_handlers = {
 
+                fill_provinces:  function(){
+
+                    var region_code = $(this).val();
+                    $('#province').ph_locations('fetch_list', [{"region_code": region_code}]);
+                    
+                },
+
+                fill_cities: function(){
+
+                    var province_code = $(this).val();
+                    $('#city').ph_locations( 'fetch_list', [{"province_code": province_code}]);
+                },
+
+
+                fill_barangays: function(){
+
+                    var city_code = $(this).val();
+                    $('#barangay').ph_locations('fetch_list', [{"city_code": city_code}]);
+                }
+            };
+
+            $(function(){
+                $('#region').on('change', my_handlers.fill_provinces);
+                $('#province').on('change', my_handlers.fill_cities);
+                $('#city').on('change', my_handlers.fill_barangays);
+
+                $('#region').ph_locations({'location_type': 'regions'});
+                $('#province').ph_locations({'location_type': 'provinces'});
+                $('#city').ph_locations({'location_type': 'cities'});
+                $('#barangay').ph_locations({'location_type': 'barangays'});
+
+                $('#region').ph_locations('fetch_list');
+            });
     </script>
 @endsection
